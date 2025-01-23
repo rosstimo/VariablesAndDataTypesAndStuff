c1d5fa6a1eee2e21e1c0560dbb180eb6ee92271e
tim <rosstimo@isu.edu>
2025-01-23 08:57:43 -0700

    add data type/conversion example

--- VariablesAndDataTypesAndStuff/VAriablesAndDataTypesAndStuff.vb
+++ VariablesAndDataTypesAndStuff/VAriablesAndDataTypesAndStuff.vb
@@ -1,6 +1,38 @@
-﻿Module VariablesAndDataTypesAndStuff
+﻿
+Option Explicit On
+Option Strict On
+
+Module VariablesAndDataTypesAndStuff
 
     Sub Main()
+
+        Dim jellyBeans As Integer
+        Dim laffyTaffy As Integer
+        Dim money As Decimal
+        Dim studentName As String
+
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = 120
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans = jellyBeans + 10
+        Console.WriteLine(jellyBeans)
+
+        jellyBeans += 10
+        Console.WriteLine(jellyBeans)
+
+        laffyTaffy = 26
+        Console.WriteLine(laffyTaffy)
+
+        money = 9.12837490812374D
+        jellyBeans = CInt(1.56)
+        studentName = "7"
+
+        jellyBeans = CInt(studentName)
+
+
+        Console.WriteLine()
 
     End Sub
 
